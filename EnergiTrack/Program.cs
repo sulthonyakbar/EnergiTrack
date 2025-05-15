0b7001b192469bbddfb6c222f36f92ae0308ba10
sahityadisini <103116839+Sahitya-RPL-21@users.noreply.github.com>
2025-05-15 21:53:22 +0700

    modul kategori
    sahit ganteng

--- EnergiTrack/Program.cs
+++ EnergiTrack/Program.cs
@@ -1,1 +1,65 @@
 ﻿
+                Console.Write("Pilih: ");
+namespace LabReservationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var categoryService = new LabReservationSystem.Services.CrudService<LabReservationSystem.Domain.Category>();
+
+            while (true)
+            {
+                Console.WriteLine("\n=== CRUD Kategori ===");
+                Console.WriteLine("1. Tambah Kategori");
+                Console.WriteLine("2. Lihat Semua Kategori");
+                Console.WriteLine("3. Edit Kategori");
+                Console.WriteLine("4. Hapus Kategori");
+                Console.WriteLine("0. Keluar");
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.Write("Nama Kategori: ");
+                        var name = Console.ReadLine();
+                        categoryService.Add(new LabReservationSystem.Domain.Category { Name = name });
+                        Console.WriteLine("Kategori berhasil ditambahkan!");
+                        break;
+
+                    case "2":
+                        var categories = categoryService.GetAll();
+                        Console.WriteLine("\n--- Daftar Kategori ---");
+                        foreach (var cat in categories)
+                        {
+                            Console.WriteLine($"ID: {cat.Id}, Nama: {cat.Name}");
+                        }
+                        break;
+
+                    case "3":
+                        Console.Write("ID Kategori: ");
+                        int editId = int.Parse(Console.ReadLine()!);
+                        Console.Write("Nama Baru: ");
+                        var newName = Console.ReadLine();
+                        categoryService.Edit(editId, new LabReservationSystem.Domain.Category { Id = editId, Name = newName });
+                        Console.WriteLine("Kategori berhasil diubah!");
+                        break;
+
+                    case "4":
+                        Console.Write("ID Kategori: ");
+                        int deleteId = int.Parse(Console.ReadLine()!);
+                        categoryService.Delete(deleteId);
+                        Console.WriteLine("Kategori berhasil dihapus!");
+                        break;
+
+                    case "0":
+                        return;
+
+                    default:
+                        Console.WriteLine("Pilihan tidak valid!");
+                        break;
+                }
+            }
+        }
+    }
+}
--- EnergiTrack/obj/Release/net8.0/EnergiTrack.AssemblyInfo.cs
+++ EnergiTrack/obj/Release/net8.0/EnergiTrack.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnergiTrack")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b22b035efda4917a91d6949c02595329b4e94ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnergiTrack")]
